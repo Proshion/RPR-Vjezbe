f791767ecfc062a015ca5c93fd8566d7c93b2ca0
Proshion <hkumalic1@etf.unsa.ba>
2023-10-24 14:22:36 +0200

    uradjen zadatak1

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,19 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Unesite broj n: ");
-        int n = scanner.nextInt();
 
-        System.out.println("Brojevi između 1 i " + n + " koji su djeljivi sa sumom svojih cifara su:");
 
-        for (int i = 1; i <= n; i++) {
-            int suma = sumaCifara(i);
-            if (i % suma == 0) {
-                System.out.print(i + " ");
-            }
-        }
+        Scanner input = new Scanner(System.in);
 
-        System.out.println();
-    }
+        System.out.println("Unesite prvi cijeli broj: ");
+        int prviBroj = input.nextInt();
 
-    public static int sumaCifara(int broj) {
-        int suma = 0;
-        while (broj > 0) {
-            suma += broj % 10;
-            broj /= 10;
-        }
-        return suma;
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Unesite drugi cijeli broj: ");
+        int drugiBroj = input2.nextInt();
+
+        System.out.println("Unijeli ste prvi broj: " + prviBroj);
+        System.out.println("Unijeli ste drugi broj: " + drugiBroj);
+
     }
 }
