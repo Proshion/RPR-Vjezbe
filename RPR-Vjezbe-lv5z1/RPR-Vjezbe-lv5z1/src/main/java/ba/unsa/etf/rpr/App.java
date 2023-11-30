34dec1c1eb12eac6cf2cbbedb657814065f532a8
Proshion <148569395+Proshion@users.noreply.github.com>
2023-11-30 15:57:47 +0100

    Delete RPR-Vjezbe-lv5z1/RPR-Vjezbe-lv5z1 directory

--- RPR-Vjezbe-lv5z1/RPR-Vjezbe-lv5z1/src/main/java/ba/unsa/etf/rpr/App.java
+++
@@ -1,8 +0,0 @@
-package ba.unsa.etf.rpr;
-
-public class App {
-    public static void main(String[] args) {
-        Laptop l=new LaptopBuilder("MacBook", "7").cijena(2000).velicinaEkrana(20).build();
-        System.out.println(l);
-    }
-}
